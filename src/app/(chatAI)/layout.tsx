47b487b1d9342b644265a3a4dc7ceaa922b2b7be
good8629 <good8629@gmail.com>
2025-02-06 21:35:05 +0900

    Update layout.tsx
    layer 파일 재수정

--- src/app/(chatAI)/layout.tsx
+++ src/app/(chatAI)/layout.tsx
@@ -2,7 +2,7 @@ import {ReactNode} from "react";
 
 interface LayoutProps {
     children: ReactNode;
-    modal?: ReactNode;
+    modal: ReactNode;
 }
 export default function Layout({ children, modal } : LayoutProps) {
     return (

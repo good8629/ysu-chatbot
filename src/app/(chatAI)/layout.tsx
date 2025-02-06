e0b9b1bf3f6d352674e256320dbe6a4fe54f29fa
good8629 <good8629@gmail.com>
2025-02-06 21:32:20 +0900

    Update layout.tsx
    수정

--- src/app/(chatAI)/layout.tsx
+++ src/app/(chatAI)/layout.tsx
@@ -1,7 +1,10 @@
 import {ReactNode} from "react";
 
-type Props = { children: ReactNode, modal: ReactNode };
-export default function Layout({ children, modal } : Props) {
+interface LayoutProps {
+    children: ReactNode;
+    modal?: ReactNode;
+}
+export default function Layout({ children, modal } : LayoutProps) {
     return (
         <div>
             {children}
